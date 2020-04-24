29023580d485a1dde5279c65329fd8150b36b715
yuriyyashkin <59850138+yuriyyashkin@users.noreply.github.com>
2020-04-24 22:16:11 +0300

    1

---
+++ DemoConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
